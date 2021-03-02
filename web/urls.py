fba5505552a9c329188917d43e37a49e86f86ec1
currodmmoguer <currodmmoguer@gmail.com>
2021-03-02 19:48:57 +0100

    Plantilla jerarquica y ayuda de busqueda

--- web/urls.py
+++ web/urls.py
@@ -6,4 +6,5 @@ from django.conf import settings
 urlpatterns = [
     path('', views.index, name="index"),
     path('persona', views.persona, name="persona"),
+    path('ajax/ayud_persona', views.ayuda_busqueda_persona, name='busqueda_persona'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- web/views.py
+++ web/views.py
@@ -1,9 +1,20 @@
+import json
 from django.shortcuts import render
+from django.http import JsonResponse, HttpResponse
+from django.core import serializers
+from .models import *
 
 def index(request):
-    return render(request, 'search.html', {})
+    return render(request, 'index.html', {})
 
 def persona(request):
-    return render(request, 'persona.html', {})
+    return render(request, 'person.html', {})
 
+# AJAX
+
+def ayuda_busqueda_persona(request):
+    personas = Persona.objects.all()
+    tmpJson = serializers.serialize("json",personas)
+    tmpObj = json.loads(tmpJson)
+    return HttpResponse(json.dumps(tmpJson))
 
